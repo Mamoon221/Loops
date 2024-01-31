fefc31ca7940a5dffb7f391a20a57d9889de04ab
Mamoon221 <mamoonmoon33@gmail.com>
2024-01-31 12:00:51 -0800

    ty

---
+++ loop.js
@@ -0,0 +1,23 @@
+// for(let i=0; i<=1000; i++){
+//     console.log(i);
+// }
+
+// for(let i=1000; i>=0; i-- ){
+//     console.log(i);
+// }
+
+// for(let i = 1; i<=5; i++){
+//     for(j = 0; j<=10; j++)
+//     console.log(`${i} x ${j} = ${i*j}`);
+// }
+
+// let arr = ['Pakistan', 'England', 'South Africa', 'Australia', 'West Indies', 'New zealand', 'India'];
+// for(let i=0; i<arr.length; i++){
+//     console.log( arr[i]);
+// }
+
+// let arr = ['Pakistan', 'England', 'South Africa', 'Australia', 'West Indies', 'New zealand', 'India'];
+// arr.reverse();
+// for(let i=0; i<arr.length; i++){
+//     console.log( arr[i]);
+// }
